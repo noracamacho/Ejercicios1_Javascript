08ea981a624846e02ac51d02d15998481381a15a
noracamacho <ingnorac@gmail.com>
2024-03-02 22:55:32 -0600

    Change: optimizing code

--- 23_printSquare.js
+++ 23_printSquare.js
@@ -2,15 +2,15 @@ import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
 const topBottom = (num, bottom = false) => {
-    let row = '\n' + '* '.repeat(num) + `${bottom === true ? '\n' : ''}`;
-    return row
+    return  '\n' + '* '.repeat(num) + `${bottom === true ? '\n' : ''}`;
 };
 
 const createSquare = (num) => {
-    let row = topBottom(num);
-    row += ('\n*' + '  '.repeat(num - 2) + ' *').repeat(num - 2);
-    row += topBottom(num, true);
-    return row
+    if(num < 2) return 'El cuadro debe contener al menos 2 caracteres por lado.';
+    let square = topBottom(num);
+    square += ('\n*' + '  '.repeat(num - 2) + ' *').repeat(num - 2);
+    square += topBottom(num, true);
+    return square
 }
 console.clear();
 const number = +prompt('Ingresa el numero de caracteres que deseas que tenga de lado el cuadrado: ');
