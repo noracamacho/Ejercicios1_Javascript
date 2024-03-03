c65be42f70fb256d3e8dacf7a11df259c1b8d682
noracamacho <ingnorac@gmail.com>
2024-03-02 22:37:37 -0600

    Change: optimizing code

--- 23_printSquare.js
+++ 23_printSquare.js
@@ -1,33 +1,17 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
-const topBottom = (side) => {
-    let row = '';
-    for(let i = 0; i < side; i++) {
-        row += '* '
-    }
-    console.log(row);
+const topBottom = (num, bottom = false) => {
+    let row = '\n' + '* '.repeat(num) + `${bottom === true ? '\n' : ''}`;
+    return row
 };
 
-const lats = (side) => {
-    for(let i = 0; i < side - 2; i++) {
-        let row = '*';
-        for(let space = 0; space < side - 2; space++) {
-            row += '  ';
-        }
-        row += ' *';
-        console.log(row);
-    }
-}
-
 const createSquare = (num) => {
-    console.log('\n');
-    topBottom(side);
-    lats(side);
-    topBottom(side);
-    console.log('\n');
+    let row = topBottom(num);
+    row += ('\n*' + '  '.repeat(num - 2) + ' *').repeat(num - 2);
+    row += topBottom(num, true);
+    return row
 }
-
 console.clear();
-const side = +prompt('Ingresa el numero de caracteres que deseas que tenga de lado el cuadrado: ');
-createSquare(side);
+const number = +prompt('Ingresa el numero de caracteres que deseas que tenga de lado el cuadrado: ');
+console.log(createSquare(number));
